a1314810b166ef06b1e8d4969e821c21a8979a2a
C Smith <csmith@oxfordquantumcircuits.com>
2024-11-01 11:16:30 +0000

    Updated harpoon and added lsp with basic setup.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,1 +1,41 @@
-return {"VonHeikemen/lsp-zero.nvim", branch = "v4.x", dependencies = {'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/nvim-cmp', "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim"}}
+return {
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v4.x",
+	dependencies = {
+		'neovim/nvim-lspconfig',
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/nvim-cmp',
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim"
+	},
+        config = function()
+		require('mason').setup({})
+		require('mason-lspconfig').setup({
+			ensure_installed = {'lua_ls', 'rust_analyzer', 'clangd'},
+			handlers = {
+				function(server_name)
+					require('lspconfig')[server_name].setup({})
+				end,
+		}})
+		local cmp = require('cmp')
+		local cmp_select = {behavior = cmp.SelectBehavior.Select}
+		cmp.setup({
+			sources = {
+				{name = 'nvim_lsp'},
+			},
+			mapping = cmp.mapping.preset.insert({
+				['<C-y>'] = cmp.mapping.confirm({select = true}),
+				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+				["<C-Space>"] = cmp.mapping.complete(),
+
+				-- scroll up and down the documentation window
+				['<C-u>'] = cmp.mapping.scroll_docs(-4),
+				['<C-d>'] = cmp.mapping.scroll_docs(4),
+			}),
+		})
+		vim.diagnostic.config({
+			virtual_text = true
+		})
+	end
+}
