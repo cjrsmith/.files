6a4adc5a2a671d46d1b3f1bad0a6884f96f9bd92
C Smith <csmith@oxfordquantumcircuits.com>
2024-11-02 21:47:16 +0000

    Added remap for searching git commits with telescope.

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
+		vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = 'Telescope git commits' })
 		vim.keymap.set('n', '<leader>ps', function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)
