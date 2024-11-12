04e8219c008465743cdd8452ffb6361b18b81d44
C Smith <csmith@oxfordquantumcircuits.com>
2024-11-12 16:47:00 +0000

    Updated cmp to use tab to cycle through auto completion and Enter to confirm.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -36,14 +36,27 @@ return {
 				{name = 'nvim_lsp'},
 			},
 			mapping = cmp.mapping.preset.insert({
-				['<C-y>'] = cmp.mapping.confirm({select = true}),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+				['<CR>'] = cmp.mapping.confirm({select = true}),
 				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-				["<C-Space>"] = cmp.mapping.complete(),
-
-				-- scroll up and down the documentation window
-				['<C-u>'] = cmp.mapping.scroll_docs(-4),
-				['<C-d>'] = cmp.mapping.scroll_docs(4),
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
 			}),
 		})
 		vim.diagnostic.config({
