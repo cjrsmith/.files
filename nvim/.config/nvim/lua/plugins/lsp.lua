3a8d0dc1c66236b798a322b2dd8e615689d43bf5
C Smith <csmith@oxfordquantumcircuits.com>
2024-12-09 23:06:59 +0000

    Added luasnip support and netrw icons

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,9 @@ return {
 	branch = "v4.x",
 	dependencies = {
 		'neovim/nvim-lspconfig',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+        'rafamadriz/friendly-snippets',
 		'hrsh7th/cmp-nvim-lsp',
 		'hrsh7th/nvim-cmp',
 		"williamboman/mason.nvim",
@@ -30,10 +33,26 @@ return {
                 end
 		}})
 		local cmp = require('cmp')
+        require('luasnip.loaders.from_vscode').lazy_load()
 		local cmp_select = {behavior = cmp.SelectBehavior.Select}
 		cmp.setup({
+            snippet = {
+                -- REQUIRED - you must specify a snippet engine
+                expand = function(args)
+                    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                    -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+                end,
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
 			sources = {
 				{name = 'nvim_lsp'},
+				{name = 'luasnip'},
 			},
 			mapping = cmp.mapping.preset.insert({
                 ["<Tab>"] = cmp.mapping(function(fallback)
