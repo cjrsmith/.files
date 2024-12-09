7c83c95add38f71d7900f43085cf0933bb274c32
C Smith <csmith@oxfordquantumcircuits.com>
2024-12-09 19:41:53 +0000

    Added lualine theme

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,23 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            -- extensions = {
+            --     "oil",
+            --     "toggleterm"
+            -- },
+            options = {
+                theme = "catppuccin",
+            },
+            sections = {
+                lualine_a = {'mode', 'searchcount'},
+                lualine_b = {'branch', 'diff'},
+                lualine_c = {'diagnostics', 'filename'},
+                lualine_x = {'encoding', 'fileformat', 'filetype'},
+                lualine_y = {'filesize'},
+                lualine_z = {'progress', 'location'}
+            },
+        })
+    end,
+}
