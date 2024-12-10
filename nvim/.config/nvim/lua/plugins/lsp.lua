e47d21007418bb90245ebac82ce10b69c51582d6
C Smith <csmith@oxfordquantumcircuits.com>
2024-12-10 13:50:40 +0000

    Formatted files

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,25 +1,25 @@
 return {
-	"VonHeikemen/lsp-zero.nvim",
-	branch = "v4.x",
-	dependencies = {
-		'neovim/nvim-lspconfig',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v4.x",
+    dependencies = {
+        'neovim/nvim-lspconfig',
         'L3MON4D3/LuaSnip',
         'saadparwaiz1/cmp_luasnip',
         'rafamadriz/friendly-snippets',
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/nvim-cmp',
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim"
-	},
-        config = function()
-		require('mason').setup({})
-		require('mason-lspconfig').setup({
-			ensure_installed = {'lua_ls', 'rust_analyzer', 'clangd'},
-			handlers = {
-				function(server_name)
-					require('lspconfig')[server_name].setup({})
-				end,
-                ["lua_ls"] = function ()
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/nvim-cmp',
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim"
+    },
+    config = function()
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            ensure_installed = { 'lua_ls', 'rust_analyzer', 'clangd' },
+            handlers = {
+                function(server_name)
+                    require('lspconfig')[server_name].setup({})
+                end,
+                ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
                     lspconfig.lua_ls.setup {
                         settings = {
@@ -31,11 +31,12 @@ return {
                         }
                     }
                 end
-		}})
-		local cmp = require('cmp')
+            }
+        })
+        local cmp = require('cmp')
         require('luasnip.loaders.from_vscode').lazy_load()
-		local cmp_select = {behavior = cmp.SelectBehavior.Select}
-		cmp.setup({
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        cmp.setup({
             snippet = {
                 -- REQUIRED - you must specify a snippet engine
                 expand = function(args)
@@ -50,11 +51,11 @@ return {
                 completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
             },
-			sources = {
-				{name = 'nvim_lsp'},
-				{name = 'luasnip'},
-			},
-			mapping = cmp.mapping.preset.insert({
+            sources = {
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+            },
+            mapping = cmp.mapping.preset.insert({
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
@@ -69,17 +70,17 @@ return {
                         fallback()
                     end
                 end, { "i", "s" }),
-				['<CR>'] = cmp.mapping.confirm({select = true}),
-				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-e>'] = cmp.mapping.abort(),
-			}),
-		})
-		vim.diagnostic.config({
-			virtual_text = true
-		})
-	end
+            }),
+        })
+        vim.diagnostic.config({
+            virtual_text = true
+        })
+    end
 }
