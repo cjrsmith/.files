22a2cd825c3b164e422c232b945216d5b83bef4a
C Smith <csmith@oxfordquantumcircuits.com>
2024-12-10 13:23:00 +0000

    Formatted treesitter file

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,16 +3,15 @@ return {
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "haskell", "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-          sync_install = false,
-	  auto_install = true,
-          highlight = { enable = true },
-          indent = { enable = true },
+        configs.setup({
+            ensure_installed = { "haskell", "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+            sync_install = false,
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
         })
     end
 }
-
