6ea78bbc42f0200eff771d2582efe5e518ed8694
C Smith <csmith@oxfordquantumcircuits.com>
2024-12-10 11:48:43 +0000

    Added buffers shortcut

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>t", ":Neotree toggle<CR>")
+vim.keymap.set("n", "<leader>b", ":Neotree toggle buffers<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
