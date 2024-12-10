15e338cebc3e1e193945674cdd21d6470616266d
C Smith <csmith@oxfordquantumcircuits.com>
2024-12-10 12:38:49 +0000

    Added transparency

--- nvim/.config/nvim/lua/plugins/colour.lua
+++ nvim/.config/nvim/lua/plugins/colour.lua
@@ -1,18 +1,42 @@
 function GimmeeColours(colour)
-	colour = colour or "catppuccin-mocha"
-	vim.cmd.colorscheme(colour)
+    colour = colour or "catppuccin-mocha"
+    vim.cmd.colorscheme(colour)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-	"catppuccin/nvim",
-    name = "catppuccin",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		GimmeeColours()
-	end
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            GimmeeColours()
+        end
+    },
+    {
+        "xiyaowong/transparent.nvim",
+        lazy = false,
+        config = function()
+            require("transparent").setup({
+                -- table: default groups
+                groups = {
+                    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+                    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+                    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+                    'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+                    'EndOfBuffer', 'NeoTreeNormal', 'NeoTreeNormalNC',
+                },
+                -- table: additional groups that should be cleared
+                extra_groups = {},
+                -- table: groups you don't want to clear
+                exclude_groups = {},
+                -- function: code to be executed after highlight groups are cleared
+                -- Also the user event "TransparentClear" will be triggered
+                on_clear = function() end,
+            })
+        end
+    }
 }
-
