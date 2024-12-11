c7c0a4f4f3b25fe3a59602138f3d90fb20bb3d5f
C Smith <csmith@oxfordquantumcircuits.com>
2024-12-11 14:41:39 +0000

    Added shortcut for focusing neo tree

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>t", ":Neotree toggle<CR>")
 vim.keymap.set("n", "<leader>b", ":Neotree toggle buffers<CR>")
+vim.keymap.set("n", "<leader>-", ":Neotree focus<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
