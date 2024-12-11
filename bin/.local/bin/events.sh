ae62a5804c0a79299ac1fd878d35c77970cfbec0
C Smith <csmith@oxfordquantumcircuits.com>
2024-12-11 13:13:27 +0000

    Updated events script

--- bin/.local/bin/events.sh
+++ bin/.local/bin/events.sh
@@ -6,11 +6,12 @@ christmas () {
     local days_left=""
     local day=$(date "+%-d")
     local remaining=$((25 - $day))
-    for ((i = 0 ; i < $remaining ; i++)); do
-        days_left+="󰓎 "
-    done
+    # for ((i = 0 ; i < $remaining ; i++)); do
+    #     days_left+="󰓎 "
+    # done
     if [ $remaining -ge 0 ]; then
-        echo $days_left"... 󰜗 󰐅 Christmas 󰒷 󰊡 "
+        # echo $days_left"... 󰜗 󰐅 Christmas 󰒷 󰊡 "
+        echo $remaining" days till... 󰜗 󰐅 Christmas 󰒷 󰊡 "
     else
         echo "󰜗 󰐅 Christmas 󰒷 󰊡 " 
     fi
@@ -20,11 +21,12 @@ halloween () {
     local days_left=""
     local day=$(date "+%-d")
     local remaining=$((31 - $day))
-    for ((i = 0 ; i < $remaining ; i++)); do
-        days_left+="󰚌 "
-    done
+    # for ((i = 0 ; i < $remaining ; i++)); do
+    #     days_left+="󰚌 "
+    # done
     if [ $remaining -ge 0 ]; then
-        echo $days_left"... 󰮣 Halloween 󰊠 "
+        # echo $days_left"... 󰮣 Halloween 󰊠 "
+        echo $remaining" days till... 󰮣 Halloween 󰊠 "
     else
         echo "󰮣 Halloween 󰊠 "
     fi
